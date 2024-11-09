2f7a763cec24ba319fc8594709adcb8df3a1cc7b
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2024-11-08 20:27:08 -0500

    Update Main Menu

---
+++ Assets/Scripts/ExplodingCow.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodingCow : MonoBehaviour
+{
+    [SerializeField] GameObject explosion;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(explosion, collision.collider.transform.position, Quaternion.identity);
+    }
+}
